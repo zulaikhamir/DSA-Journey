3d21d2efe403c1bb05bb21213c0048950e8202bc
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-18 02:30:23 +0530

    .

---
+++ day3/factorial.java
@@ -0,0 +1,24 @@
+// public class ok{
+// 	public static void main(String[] args){
+// 		int n=5;
+
+// 		int result=1;
+// 		for(int i=2;i<=n;i++){
+// 			result=result*i;
+// 		}
+// 		System.out.println("The factorial of " + n + " is: " + result);
+
+// 	}
+// }
+public class factorial{
+
+	public static int fact(int n){
+		if(n==0 || n==1)
+		return 1;
+		return n * fact(n-1);
+	}
+	public static void main(String[] args){
+	int n=20;
+	System.out.println("factorial is :"+fact(n));
+	}
+}
--- day3/noofdigits.java
+++ day3/noofdigits.java
@@ -1,13 +1,22 @@
 public class noofdigits{
-	public static void main(String[] args){
-		int n=123456789;
-		int count=0;
-		while(n!=0){
-			n=n/10;
-			count++;
-			
+
+	public static int digits(int n){
+		if(n==0){
+          return 0;
 		}
-		System.out.println(count);
+		return 1 + digits(n/10);
+	}
+	public static void main(String[] args){
+		// int n=123456789;
+		// int count=0;
+		// while(n!=0){
+		// 	n=n/10;
+		// 	count++;
+			
+		// }
+		// System.out.println(count);
+		int n=2345;
+		System.out.println("no of digits are: "+digits(n));
 
 	}
 
