ad147c00c6653378874617d4c96fa593d583e9bb
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-17 16:56:39 +0530

    .

--- Day-1/main.java
+++ Day-1/main.java
@@ -1,5 +1,13 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Alhamdulillah, Java is working!");
+
+    int sum=0;
+    int n=5;
+    int i=0;
+   // sum = n*(n+1)/2;
+  for(i=0;i<=n;i++){
+    sum=sum+i;
+  }
+        System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
     }
 }
---
+++ Day-2/Arraysum.java
@@ -0,0 +1,20 @@
+public class Arraysum {
+    public static void main(String[] args) {
+
+    int sum=0;
+    int n=5;
+    int arr[]={10,20,30,60,100};
+    // int arr[] = new int[5];
+    // arr[0] = 10;
+    // arr[1] = 20;
+    // arr[2] = 30;
+    // arr[3] = 40;
+    // arr[4] = 50;
+      
+	   int i=0;
+  for(i=0;i<n;i++){
+    sum=sum+arr[i];
+  }
+        System.out.println("The sum of elements of array of size  " + n + "  is: " + sum);
+    }
+}
