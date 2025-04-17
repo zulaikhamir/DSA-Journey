c53b94ec73ce62d7192dc63170c27bb169c6599f
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-18 02:08:45 +0530

    .

---
+++ day3/noofdigits.java
@@ -0,0 +1,15 @@
+public class noofdigits{
+	public static void main(String[] args){
+		int n=123456789;
+		int count=0;
+		while(n!=0){
+			n=n/10;
+			count++;
+			
+		}
+		System.out.println(count);
+
+	}
+
+
+	}
