5c7a6f30f76b8f366d38ba16230ad8eba057a33e
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-18 23:54:52 +0530

    recursion

---
+++ day5/binary.java
@@ -0,0 +1,15 @@
+public class binary{
+
+	static void fun(int n){
+		if(n<=0)
+		return;
+		fun(n/2);
+		System.out.println(n%2);
+
+	}
+
+	public static void main(String[] args) {
+		int n=7;
+		fun(n);
+	}
+}
