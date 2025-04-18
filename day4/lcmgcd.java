05e522fdcf6be42814b2867abf1b9d5cd14dd109
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-18 19:38:27 +0530

    .

---
+++ day4/lcmgcd.java
@@ -0,0 +1,19 @@
+public class lcmgcd{
+
+static int gcd(int a,int b){
+	if(b==0)
+	return a;
+	else{
+		return gcd(b,a%b);
+	}
+
+}
+static int lcm(int a,int b){
+	return a*b/gcd(a, b);
+}
+	public static void main(String[] args) {
+		int a=12;
+		int b=24;
+		System.out.println("lcm is: "+lcm(a,b));
+}
+}
