607a6fafbbe992c3504083ccb16956cd2c0e59b3
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-19 00:47:10 +0530

    .

---
+++ day5/honai.java
@@ -0,0 +1,22 @@
+public class honai{
+
+
+	static void tower(int n, char A,char B,char C){
+		if(n==1){
+			System.out.println("move disc "+n+ " from " +A+" to "+B);
+			return;
+		}
+		//our mission is to ove from a to b
+		tower(n-1,A,C,B);//move a to c using b as aux
+		System.out.println("move disc "+n+ " from " +A+" to "+B);
+		tower(n-1,C,B,A);//mov c to b using a as aux
+	}
+	public static void main(String[] args) {
+
+		int n=3;
+		char a='A';
+		char b='B';
+		char c='C';
+		tower(n,a,b,c);
+	}
+}
--- day5/oneton.java
+++ day5/oneton.java
@@ -6,6 +6,7 @@ public class oneton{
 		
 		print(n-1);
 		System.out.println(n+"");
+		//print(n-1);//tail recuriosn if recursive call is the last call in a function
 	}
 	public static void main(String[] args) {
 		int n=8;
