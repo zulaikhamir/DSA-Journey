395227df372fb316a09f158058185967afe40506
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-20 20:31:32 +0530

    .

---
+++ day7/anagram.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public  class anagram{
+	public static void main(String[] args) {
+		String s1="abcdf";
+		String s2="acbfed";
+		if(s1.length()!=s2.length())
+		System.out.println("no anagram");
+
+		char a[]=s1.toCharArray();
+		Arrays.sort(a);
+		s1= new String(a);
+
+		char b[]=s2.toCharArray();
+		Arrays.sort(b);
+		s2= new String(b);
+
+		if(s1.equals(s2))
+		System.out.println("anagram");
+
+	}
+}
