743e8df03ba35be51da5dad3aa45c7a2e09c0799
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-21 23:05:55 +0530

    .

---
+++ patterns/halfpyramid.java
@@ -0,0 +1,11 @@
+public  class halfpyramid{
+	public static void main(String[] args) {
+		int m=5;
+		for(int i=0;i<m;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
