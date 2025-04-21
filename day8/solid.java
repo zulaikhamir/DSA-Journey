86298414d43ebfe7f0ac352a6fe4b19a60f56d6d
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-21 22:43:08 +0530

    .

---
+++ day8/solid.java
@@ -0,0 +1,12 @@
+public class solid{
+	public static void main(String[] args) {
+		int m=4;
+		int n=8;
+		for(int i=0;i<m;i++){
+			for(int j=0;j<n;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
