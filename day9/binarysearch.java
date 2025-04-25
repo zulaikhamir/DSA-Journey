e20205ed4a8ef7ff6086e6ca1f534d9ffbea81b8
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-04-25 22:15:25 +0530

    .

---
+++ day9/binarysearch.java
@@ -0,0 +1,31 @@
+public class binarysearch{
+	public static void main(String[] args) {
+		int [] arr={10, 20, 30, 40, 50, 60, 70};
+		int low=0;
+		int n=arr.length;
+		int high=n-1;
+		int k=20;
+
+		while(low<=high){
+			int  mid=(low+high)/2;
+
+			if(arr[mid]==k){
+				System.out.println("found at "+mid);
+				return;
+			}
+
+			
+			else{
+				if(arr[mid]<k){
+					low=mid+1;
+				}
+			
+			else {
+				high=mid-1;
+			}
+			
+			}
+			
+		}
+	}
+}
