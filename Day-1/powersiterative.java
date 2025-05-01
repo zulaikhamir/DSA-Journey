900169c1ef69ea10bea2b07d72f368b30b9cf6ec
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-01 21:22:44 +0530

    .

---
+++ Day-1/powersiterative.java
@@ -0,0 +1,30 @@
+
+public class PowersIterative {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 3;
+        int res = power(x, n);
+        System.out.println(res); // Output: 8
+    }
+
+    // Efficient iterative power function using exponentiation by squaring
+    static int power(int x, int n) {
+        int res = 1;
+
+        while (n > 0) {
+            // If the current bit of n is set (i.e., n is odd)
+            if ((n & 1) == 1) {
+                res *= x;
+            }
+
+            // Square the base
+            x *= x;
+
+            // Move to next bit by dividing n by 2
+            n >>= 1; // same as n = n / 2;
+        }
+
+        return res;
+    }
+}
