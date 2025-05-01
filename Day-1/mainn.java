55e037ad65e8cd39ddd2b01a71fdbd9dc7304ada
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-01 20:45:48 +0530

    .

--- Day-1/mainn.java
+++ Day-1/mainn.java
@@ -1,11 +1,10 @@
-public class main {
+public class mainn {
     public static void main(String[] args) {
 
     int sum=0;
     int n=5;
-    int i=0;
    // sum = n*(n+1)/2;
-  for(i=0;i<=n;i++){
+  for(int i=0;i<=n;i++){
     sum=sum+i;
   }
         System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
---
+++ Day-1/powers.java
@@ -0,0 +1,17 @@
+
+public class powers {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 3;
+        int res = 1;
+
+        for (int i = 0; i < n; i++) {
+            res = res * x;
+
+        }
+        System.out.println(res);
+
+    }
+
+}
