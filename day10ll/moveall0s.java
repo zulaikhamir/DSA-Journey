9ce429beefc153e0d9ae0f559d69d26f5e7cb7e8
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-03 20:19:43 +0530

    .

--- day10ll/moveall0s.java
+++ day10ll/moveall0s.java
@@ -1,4 +1,24 @@
 
+// public class moveall0s {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 0, 4, 0, 5, 6, 0, 0, 7, 8};
+//         int n = arr.length;
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] == 0) {
+//                 for (int j = i + 1; j < n; j++) {
+//                     if (arr[j] != 0) {
+//                         int temp = arr[i];
+//                         arr[i] = arr[j];
+//                         arr[j] = temp;
+//                     }
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < n; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
 public class moveall0s {
 
     public static void main(String[] args) {
@@ -6,22 +26,18 @@ public class moveall0s {
         int[] arr = {1, 2, 0, 4, 0, 5, 6, 0, 0, 7, 8};
         int n = arr.length;
 
+        int count = 0;
         for (int i = 0; i < n; i++) {
-
-            if (arr[i] == 0) {
-                for (int j = i + 1; j < n; j++) {
-                    if (arr[j] != 0) {
-                        int temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-
-                    }
-                }
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
             }
         }
+
         for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
-
         }
 
     }
