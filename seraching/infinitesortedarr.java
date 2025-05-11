511380b77852560c27e0027ba7769e86bd099fb7
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-11 13:31:00 +0530

    .

---
+++ seraching/infinitesortedarr.java
@@ -0,0 +1,31 @@
+
+public class infinitesortedarr {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 10, 15, 20, 40, 60, 90, 100, 120, 500};
+        int z = 70;
+
+        int x = search(arr, z);
+        if (x > 0) {
+            System.out.println("found at: " + x);
+        } else {
+            System.out.println("not present");
+        }
+
+    }
+
+    static int search(int[] arr, int z) {
+        int i = 0;
+        while (true) {
+            if (arr[i] == z) {
+                return i;
+            }
+            if (arr[i] > z) {
+                return -1;
+            }
+            i++;
+        }
+
+    }
+
+}
