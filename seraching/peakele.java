d26bf369799789be49becccff0b216080e4c5513
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-11 19:11:54 +0530

    .

--- seraching/peakele.java
+++ seraching/peakele.java
@@ -11,24 +11,43 @@ public class peakele {
             System.out.println("mo peak ele");
         }
     }
-//The linear search approach (like your code) will return the first peak it encounters.
 
     static int getpeak(int[] arr, int n) {
-        if (n == 1) {
-            return arr[0]; // if array has only one element
-        }
-        if (arr[0] >= arr[1]) {
-            return arr[0]; // peak is 0th element if its rhs is less
-        }
-        if (arr[n - 1] >= arr[n - 2]) {
-            return arr[n - 1]; // peak is last element
-        }
-
-        for (int i = 1; i < n - 1; i++) {
-            if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
-                return arr[i]; // ele who has smaller to it nos on both sides
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if ((mid == 0 || arr[mid - 1] <= arr[mid]) && (mid == n - 1 || arr[mid + 1] <= arr[mid])) {
+                //if mid ist ele or mid is last ele or mid itself is the peak element 
+                return mid;
             }
+            if (mid > 0 && arr[mid - 1] >= arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
         }
         return -1;
     }
+
 }
+
+//The linear search approach (like your code) will return the first peak it encounters.
+// static int getpeak(int[] arr, int n) {
+// 	if (n == 1) {
+// 		return arr[0]; // if array has only one element
+// 	}
+// 	if (arr[0] >= arr[1]) {
+// 		return arr[0]; // peak is 0th element if its rhs is less
+// 	}
+// 	if (arr[n - 1] >= arr[n - 2]) {
+// 		return arr[n - 1]; // peak is last element
+// 	}
+// 	for (int i = 1; i < n - 1; i++) {
+// 		if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
+// 			return arr[i]; // ele who has smaller to it nos on both sides
+// 		}
+// 	}
+// 	return -1;
+// }
