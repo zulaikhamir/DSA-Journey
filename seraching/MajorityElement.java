9460ea2064477fb765a7cf58b91cf4b38cb6c1ec
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-12 15:39:19 +0530

    .

---
+++ seraching/MajorityElement.java
@@ -0,0 +1,28 @@
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 3, 3};
+        int n = arr.length;
+        int majorityCount = n / 2;
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count > majorityCount) {
+                System.out.println("Majority element is: " + arr[i]);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No majority element");
+        }
+    }
+}
