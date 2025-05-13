14d9432945c47b0afe8af54c32b94e27c10aa386
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-13 16:24:46 +0530

    .

---
+++ matrix/EfficientMatrixRotation.java
@@ -0,0 +1,51 @@
+
+public class EfficientMatrixRotation {//only for 	squared matrix
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        rotate90Clockwise(matrix);
+
+        System.out.println("\nMatrix after 90 degree clockwise rotation:");
+        printMatrix(matrix);
+    }
+
+    public static void rotate90Clockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                // Swap matrix[i][j] with matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                // Swap matrix[i][j] with matrix[i][n-j-1]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    // Helper method to print matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
