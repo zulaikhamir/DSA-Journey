610a438e43c4782023de06eef8c87a31a1bb2cb5
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-16 00:26:05 +0530

    .

---
+++ dequeue/ArrayDequeAsQueue.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeAsQueue {
+
+    public static void main(String[] args) {
+        // Create ArrayDeque as a queue
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+        // Enqueue elements (inserting at rear)
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+
+        // Display queue elements
+        System.out.println("Queue elements (front to rear):");
+        for (int num : queue) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Peek front element
+        System.out.println("Front element: " + queue.peek());
+
+        // Dequeue elements (removing from front)
+        System.out.println("Dequeued element: " + queue.poll());
+        System.out.println("Dequeued element: " + queue.poll());
+
+        // Display remaining queue
+        System.out.println("Queue after dequeuing:");
+        for (int num : queue) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Check if queue is empty
+        System.out.println("Is queue empty? " + queue.isEmpty());
+    }
+}
