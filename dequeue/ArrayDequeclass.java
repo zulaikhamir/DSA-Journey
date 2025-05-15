d26325383b342b3ed56b977f6128f4f74804d145
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-05-16 00:20:51 +0530

    .

---
+++ dequeue/ArrayDequeclass.java
@@ -0,0 +1,45 @@
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ArrayDequeclass {
+
+    public static void main(String[] args) {
+        // Create ArrayDeque
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // Inserting elements at rear
+        deque.addLast(10);
+        deque.addLast(20);
+        deque.addLast(30);
+
+        // Inserting elements at front
+        deque.addFirst(5);
+        deque.addFirst(1);
+
+        // Traversing using for-each
+        System.out.println("Deque elements:");
+        for (int num : deque) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Accessing front and rear elements
+        System.out.println("Front element: " + deque.peekFirst());
+        System.out.println("Rear element: " + deque.peekLast());
+
+        // Deleting from front and rear
+        deque.removeFirst();
+        deque.removeLast();
+
+        // Traversing again
+        System.out.println("Deque after deletions:");
+        for (int num : deque) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Check size
+        System.out.println("Size of deque: " + deque.size());
+    }
+}
