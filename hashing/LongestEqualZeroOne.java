ebcdeb1933a7bd805c42d0dcc3b2d9aab2eae0e2
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-07-02 23:24:05 +0530

    .

---
+++ hashing/LongestEqualZeroOne.java
@@ -0,0 +1,44 @@
+
+import java.util.HashMap;
+
+public class LongestEqualZeroOne {
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 1, 0, 1, 1};
+
+        // Replace 0 with -1
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = -1;
+            }
+        }
+
+        // HashMap to store prefixSum and its first occurrence index
+        HashMap<Integer, Integer> prefixSumMap = new HashMap<>();
+
+        int prefixSum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+
+            // If prefixSum is 0, subarray from 0 to i has equal 0s and 1s
+            if (prefixSum == 0) {
+                maxLength = i + 1;
+            }
+
+            // If this prefixSum was seen before, update maxLength
+            if (prefixSumMap.containsKey(prefixSum)) {
+                int length = i - prefixSumMap.get(prefixSum);
+                if (length > maxLength) {
+                    maxLength = length;
+                }
+            } else {
+                // Store prefixSum with first occurrence index
+                prefixSumMap.put(prefixSum, i);
+            }
+        }
+
+        System.out.println("Length of longest subarray with equal number of 0s and 1s is: " + maxLength);
+    }
+}
