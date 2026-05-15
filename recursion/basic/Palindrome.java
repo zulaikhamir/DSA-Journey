171cd9e6acc9440cc72af116a59b7019f9ccac7d
Zulaikha mir <zulaikhamir786@gmail.com>
2026-05-15 11:36:05 +0530

    basic recursion

--- recursion/basic/Palindrome.java
+++ recursion/basic/Palindrome.java
@@ -18,4 +18,35 @@ boolean isPalindrome(String s, int left, int right){
         return false;
 
     return isPalindrome(s, left+1, right-1);
+    //if i do left++ or right-- then the value of left and right will be changed for the next recursive call and it will not work as expected
+}
+
+
+//
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+        if(s.length()==0 || s.length()==1) return true;
+
+        return isPalindrome(s,0,s.length()-1);
+    }
+    boolean isPalindrome(String s,int left,int right){
+        if(left>=right) return true;
+        if(s.charAt(left)!=s.charAt(right)) return false;
+
+        return isPalindrome(s,left+1,right-1);
+
+    }
+}
+
+//only one function
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+        if(s.length()==0 || s.length()==1) return true;
+
+        if(s.charAt(0)!=s.charAt(s.length()-1)) return false;
+
+        return isPalindrome(s.substring(1,s.length()-1));//however substring creates a new string each time and it is not space efficient
+    }
 }
