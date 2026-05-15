6d24a1829decc33faddcf580e704dded30f1d698
Zulaikha mir <zulaikhamir786@gmail.com>
2026-05-15 12:01:35 +0530

    tower of hanoi

--- recursion/medium/TowerOfHanoi.java
+++ recursion/medium/TowerOfHanoi.java
@@ -1,15 +1,21 @@
+//the problem is to move n discs from a to b using c as auxilary
+//the rules are only one disc can be moved at a time and each move consists of taking the upper disc from one of the stacks and placing it on top of another stack or on an empty rod
+//no disc may be placed on top of a smaller disc
+//
+
+
 public class TowerOfHanoi{
 
 
-	static void tower(int n, char A,char B,char C){
+	static void tower(int n, char A,char B,char C){//means move n discs from A to B using C as auxilary
 		if(n==1){
 			System.out.println("move disc "+n+ " from " +A+" to "+B);
 			return;
 		}
-		//our mission is to ove from a to b
+		//our mission is to move from a to b
 		tower(n-1,A,C,B);//move a to c using b as aux
-		System.out.println("move disc "+n+ " from " +A+" to "+B);
-		tower(n-1,C,B,A);//mov c to b using a as aux
+		System.out.println("move disc "+n+ " from " +A+" to "+B);//move the largest disc from a to b
+		tower(n-1,C,B,A);//move c to b using a as aux
 	}
 	public static void main(String[] args) {
 
