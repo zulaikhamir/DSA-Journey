58650615e62e989baf51dc416756d3635cfaccff
Zulaikha mir <zulaikhamir786@gmail.com>
2026-05-18 15:03:43 +0530

    backtracking

--- recursion/medium/SubsetSum.java
+++ recursion/medium/SubsetSum.java
@@ -13,10 +13,10 @@ public class SubsetSum {
     static int sum(int arr[], int n, int sum) {
 
         if (n == 0) {
-            return (sum == 0) ? 1 : 0;
+            return (sum == 0) ? 1 : 0;//if the sum is 0 then we have found a subset whose sum is equal to the given sum so we return 1 otherwise we return 0
         }
 
-        return sum(arr, n - 1, sum) + sum(arr, n - 1, sum - arr[n - 1]);//one side you include the the ele from array and subtarct and other you dont then you add tem sum results in result
-
+       return sum(arr, n - 1, sum) //not including the last element
+     + sum(arr, n - 1, sum - arr[n - 1]);//including the last element
     }
 }
